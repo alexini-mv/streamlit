14e107038522dafd93696421384cc4f857e8eec4
Alejandro Martínez-Valencia <vaf.ss4@gmail.com>
2022-05-30 05:03:25 -0500

    Agregamos pequeños comentarios al código

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 # IMPORTAMOS LOS DATOS A UN DATAFRAME
 df = pd.read_csv("data_simulation/experimento.csv", index_col="time")
 
-# PINTAMOS EN LA APLICACIÓN UNA MUESTRA DE LOS DATOS
+# MOSTRAMOS EN EL DASHBOARD UNA MUESTRA DE LOS DATOS
 st.write("""
 ## Modelo Cinético de la Liberación de Neurotransmisor
 
@@ -14,8 +14,8 @@ st.write("""
 
 st.write(df.head())
 
+# DESPLEGAMOS LOS GRÁFICOS QUE DESEAMOS MOSTRAR A PARTIR DE UN DATAFRAME
 st.write("""
-
 **Evolución temporal de la cantidad de vesiculas en cada estado.**
 """)
 
@@ -30,20 +30,3 @@ st.line_chart(df["Transition 5"])
 
 
 
-# df= pd.DataFrame({
-# "first column": [1,2,3,4],
-# "second column": [10,20,30,40]
-# })
-
-# st.write("Aquí esta mi primera tabla")
-# st.write(df)
-
-# x = st.slider("x")
-# st.write(x, "cuadrado es",x * x)
-
-# add_selectbox = st.sidebar.selectbox(
-# "How would you like to be contacted?",
-# ("Email", "Home Phone", "Mobile Phone")
-# )
-
-# add_slider = st.sidebar.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
